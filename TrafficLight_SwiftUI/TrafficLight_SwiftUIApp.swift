10bb46bae9dfb83f9278386b07c6cfcf9d1fdfd0
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-10-27 12:05:07 +0300

    Initial Commit

---
+++ TrafficLight_SwiftUI/TrafficLight_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_SwiftUIApp.swift
+//  TrafficLight_SwiftUI
+//
+//  Created by Константин Кириллов on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
